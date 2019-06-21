59a9250eb2d27955afd6b21ee027b1eed5a07f0c
杨秀丽 <you@example.com>
2019-06-21 11:01:01 +0800

    第一次登录

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,16 +7,13 @@ class Index
 {
     public function index()
     {
-    $mysql="select * from user";
-    $arr1=Db::query($mysql);
- 	var_dump($arr1);
-	 // return view("index");
+  //   $mysql="select * from user";
+  //   $arr1=Db::query($mysql);
+ 	// var_dump($arr1);
+	 return view("index");
        
     }
 
-    public function hello($name = 'ThinkPHP5')
-    {
-        return 'hello,' . $name;
-    }
+    
 }
 ?>

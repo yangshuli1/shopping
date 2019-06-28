6707474224350d17495b600c463d9b223310bdc1
杨秀丽 <you@example.com>
2019-06-28 19:08:26 +0800

    a

--- application/admin/controller/Permissioncategory.php
+++ application/admin/controller/Permissioncategory.php
@@ -13,14 +13,36 @@ class Permissioncategory extends Common
    }
     public function add()
    {
-   $name=Request::post('name');
-   $marks=Request::post('marks');
+   $data=Request::post('');
+   // var_dump($data);
+   // die;
+   	 $validate = new \app\admin\validate\permiss;
+   	  if (!$validate->check($data)) {
+   	  	$arr=['yan'=>'1','status'=>'error','data'=>$validate->getError()];
+          $json=json_encode($arr);
+	 	echo $json;
+	 	die;
+        }
+   	 
    $rbac=new Rbac;
-   $rbac->savePermissionCategory([
-    'name' => $name,
-    'description' => $marks,
+   $name=$rbac->getPermissionCategory([['name','=',$data['name']]]);
+   if (empty($name)) {
+   	$rbac->savePermissionCategory([
+    'name' => $data['name'],
+    'description' =>$data['description'],
     'status' => 1
+
 	]);
+	$arr=['yan'=>'1','status'=>'OK','data'=>"添加成功"];
+   	 $json=json_encode($arr);
+	 	echo $json;
+   }else{
+   	$arr=['yan'=>'1','status'=>'error','data'=>"类名已存在"];
+   	 $json=json_encode($arr);
+	 	echo $json;
+	 	
+   }
+   
    }
    public function insert()
    {
